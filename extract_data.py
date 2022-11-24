106931767d73d18deda6c80e99e34a742d3df6c6
arthurbr11 <arthurb242@icloud.com>
2022-11-24 15:27:31 +0100

    fonction

--- extract_data.py
+++ extract_data.py
@@ -1,43 +1,45 @@
 import tools_json
 from pprint import pprint
+def return_all_parameters():
+    brut_data = tools_json.read_json('Instances/tiny.json')
 
-brut_data = tools_json.read_json('Instances/tiny.json')
+    """Instances Parameters"""
 
-"""Instances Parameters"""
+    J = brut_data['parameters']['size']['nb_jobs']
+    I = brut_data['parameters']['size']['nb_tasks']
+    M = brut_data['parameters']['size']['nb_machines']
+    O = brut_data['parameters']['size']['nb_operators']
 
-J = brut_data['parameters']['size']['nb_jobs']
-I = brut_data['parameters']['size']['nb_tasks']
-M = brut_data['parameters']['size']['nb_machines']
-O = brut_data['parameters']['size']['nb_operators']
+    alpha = brut_data['parameters']['costs']['unit_penalty']
+    beta = brut_data['parameters']['costs']['tardiness']
 
-alpha = brut_data['parameters']['costs']['unit_penalty']
-beta = brut_data['parameters']['costs']['tardiness']
+    """Job Parameters"""
+    S = []
+    r = []
+    d = []
+    w = []
+    for j in range(J):
+        S.append(brut_data['jobs'][j]['sequence'])
+        r.append(brut_data['jobs'][j]['release_date'])
+        d.append(brut_data['jobs'][j]['due_date'])
+        w.append(brut_data['jobs'][j]['weight'])
 
-"""Job Parameters"""
-S = []
-r = []
-d = []
-w = []
-for j in range(J):
-    S.append(brut_data['jobs'][j]['sequence'])
-    r.append(brut_data['jobs'][j]['release_date'])
-    d.append(brut_data['jobs'][j]['due_date'])
-    w.append(brut_data['jobs'][j]['weight'])
+    """ Tasks i parameters and machines m in M_spaces[i] parameters"""
+    p = []
+    M_space = []
+    O_space=[]#Oim=O[i][m]
+    for i in range(I):
+        p.append(brut_data['tasks'][i]['processing_time'])
+        Mi=[]
+        Oi=[None]*M
+        for m in range(len(brut_data['tasks'][i]['machines'])):
+            index_machines=brut_data['tasks'][i]['machines'][m]['machine']
+            Mi.append(index_machines)
+            Oi[index_machines-1]=brut_data['tasks'][i]['machines'][m]['operators']
+        O_space.append(Oi)
+        M_space.append(Mi)
 
-""" Tasks i parameters and machines m in M_spaces[i] parameters"""
-p = []
-M_space = []
-O_space=[]#Oim=O[i][m]
-for i in range(I):
-    p.append(brut_data['tasks'][i]['processing_time'])
-    Mi=[]
-    Oi=[None]*M
-    for m in range(len(brut_data['tasks'][i]['machines'])):
-        index_machines=brut_data['tasks'][i]['machines'][m]['machine']
-        Mi.append(index_machines)
-        Oi[index_machines-1]=brut_data['tasks'][i]['machines'][m]['operators']
-    O_space.append(Oi)
-    M_space.append(Mi)
+    return (J,I,M,O,alpha,beta,S,r,d,w,p,M_space,O_space)
 
 
 
