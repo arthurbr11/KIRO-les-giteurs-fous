592727a64f5590bed1239e398388fa679ff11e3c
arthurbr11 <arthurb242@icloud.com>
2022-11-24 15:52:46 +0100

    field solution

--- SOLUTION.py
+++ SOLUTION.py
@@ -1,2 +1,12 @@
 import tools_json
 
+
+def solution(path,Solution_solveur):
+    solution={[]}
+
+    B,M,O=Solution_solveur[0],Solution_solveur[1],Solution_solveur[2]
+    I=len(B)
+    for i in range(I):
+        solution.append({'task':i+1,"start ":B[i],"machine ":M[i],"operator ":O[i]})
+
+    tools_json.create_json(solution,path)
