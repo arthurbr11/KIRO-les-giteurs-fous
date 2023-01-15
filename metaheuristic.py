87d2d1464a4bdaa685c0131509b6c3283eb064ea
arthurbr11 <arthurb242@icloud.com>
2023-01-15 13:34:38 +0100

    try meta

--- metaheuristic.py
+++ metaheuristic.py
@@ -7,11 +7,27 @@ from pprint import pprint
 
 import extract_data
 import tools_json
-import analysis_sol
+import analysis_sol as a
 import glouton
 import space_sol
 
 INSTANCE = glouton.INSTANCE
+
+
+def random_swap_index_user(User):  # o or m
+    i = np.random.randint(0,len(User))
+    j = np.random.randint(0,len(User))
+    while i == j or User[i] == User[j]:
+        j = np.random.randint(0,len(User))
+    return i, j
+
+
+def random_swap_index(I):
+    i = np.random.randint(0,I)
+    j = np.random.randint(0,I)
+    while i == j:
+        j = np.random.randint(0,I)
+    return i, j
 
 
 def extract_space():
@@ -20,7 +36,10 @@ def extract_space():
             'SOL/glouton_random/KIRO-medium-sol_11.json', 'SOL/glouton_random/KIRO-large-sol_11.json']
     type_data = ['tiny', 'small', 'medium', 'large']
     for k in range(4):
-        SPACE_SOL[type_data[k]].append(analysis_sol.read_solution(path[k]))
+        SPACE_SOL[type_data[k]].append(a.read_solution(path[k]))
+        SPACE_SOL[type_data[k]] = sorted(SPACE_SOL[type_data[k]],
+                                         key=lambda sol: a.cost(sol, INSTANCE[type_data[k]]))
+
     return SPACE_SOL['tiny'].copy(), SPACE_SOL['small'].copy(), SPACE_SOL['medium'].copy(), SPACE_SOL['large'].copy()
 
 
@@ -32,16 +51,77 @@ def caracteristic_space(space_sol, instance):
     best = np.infty
     worst = 0
     for s in space_sol:
-        c = analysis_sol.cost(s, instance)
+        c = a.cost(s, instance)
         avg += c / len(space_sol)
         if c < best:
             best = c
         if c > worst:
             worst = c
+
     return {'nb': len(space_sol), 'best': best, 'avg': int(avg), 'worst': worst}
 
 
-pprint(caracteristic_space(space_sol_tiny, INSTANCE['tiny']))
-pprint(caracteristic_space(space_sol_small, INSTANCE['small']))
-pprint(caracteristic_space(space_sol_medium, INSTANCE['medium']))
-pprint(caracteristic_space(space_sol_large, INSTANCE['large']))
+def mutation_swap_operator(sol, data):
+    mut_sol =  a.Solution(sol.starts,sol.machines,sol.operators)
+    while True:
+        i, j = random_swap_index_user(sol.operators)
+        mut_sol.operators[i], mut_sol.operators[j] = mut_sol.operators[j], mut_sol.operators[i]
+        if a.is_feasible(mut_sol, INSTANCE[data], False):
+            return mut_sol
+
+
+def mutation_swap_machine(sol, data):
+    mut_sol =  a.Solution(sol.starts,sol.machines,sol.operators)
+    while True:
+        i, j = random_swap_index_user(sol.machines)
+        mut_sol.machines[i], mut_sol.machines[j] = mut_sol.machines[j], mut_sol.machines[i]
+        if a.is_feasible(mut_sol, INSTANCE[data], False):
+            return mut_sol
+
+
+def mutation_swap_start(sol, data):
+    mut_sol = a.Solution(sol.starts,sol.machines,sol.operators)
+    while True:
+        i, j = random_swap_index(len(sol.starts))
+        mut_sol.machines[i], mut_sol.machines[j] = mut_sol.machines[j], mut_sol.machines[i]
+        if a.is_feasible(mut_sol, INSTANCE[data], False):
+            return mut_sol
+
+
+def create_child_1p(sol_1, sol_2, data):
+    I = len(sol_1.starts)
+    mid = I // 2
+    B1, B2, O1, O2, M1, M2 = sol_1.starts.copy(), sol_2.starts.copy(), sol_1.operators.copy(), sol_2.operators.copy(), sol_1.machines.copy(), sol_2.machines.copy()
+    child_1 = a.Solution(B1[0:mid] + B2[mid:I], M1[0:mid] + M2[mid:I], O1[0:mid] + O2[mid:I])
+    child_2 = a.Solution(B2[0:mid] + B1[mid:I], M2[0:mid] + M1[mid:I], O2[0:mid] + O1[mid:I])
+    children = []
+    if a.is_feasible(child_1, INSTANCE[data],verbose=False):
+        children.append(child_1)
+    if a.is_feasible(child_2, INSTANCE[data],verbose=False):
+        children.append(child_2)
+    return children
+
+
+def generate_new_gen(space_sol_data, data):
+    N = len(space_sol_data)
+    new_gen = space_sol_data[0:N // 10].copy()  # 10% elite old gen
+    """for _ in tqdm(range((N * 2) // 100)):  # 6% mutation on old gen
+        n1, n2, n3 = np.random.randint(0,N), np.random.randint(0,N), np.random.randint(0,N)
+        new_gen.append(mutation_swap_machine(space_sol_data[n2], data))
+        print("hello")
+        new_gen.append(mutation_swap_operator(space_sol_data[n3], data))
+        print("hello")
+        new_gen.append(mutation_swap_start(space_sol_data[n1], data))
+        print("hello")"""
+    while len(new_gen) < N:
+        n1, n2 = random_swap_index(N)
+        children = create_child_1p(space_sol_data[n1], space_sol_data[n2], data)
+        if len(children) != 0:
+            new_gen.extend(children)
+            print("add")
+    return new_gen
+
+
+print(caracteristic_space(space_sol_large, INSTANCE['large']))
+new_gen = generate_new_gen(space_sol_large, 'large')
+print(caracteristic_space(new_gen, INSTANCE['large']))
